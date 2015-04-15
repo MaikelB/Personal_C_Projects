8ef3e3ab3c3fbf9114919354f47393044e1161dd
MaikelB <maikel.bolderdijk@student.hu.nl>
2015-04-15 15:36:15 +0200

    FINISHED

--- Opdracht_1_4/opdracht_1_4.c
+++ Opdracht_1_4/opdracht_1_4.c
@@ -17,5 +17,21 @@
 #include <stdio.h>
 
 int main(void) {
+	int n;
+	char rightAlign[] = "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t";
 
+	printf("Insert a number between 0 and 100\n");
+	fflush(stdout);
+
+	while(!scanf("%d",&n)) {
+		printf("Wrong input, try again\n");
+		while(getchar() == '\n');
+		fflush(stdout);
+	}
+	int i;
+	for(i = 1; i <= 10; i++) {
+		printf("%d * %d%-10s%d\n",n,i,rightAlign,n*i);
+	}
+
+	return 0;
 }
